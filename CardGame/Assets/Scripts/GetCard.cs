ed7db8750e0656e2d1d5d6f5b75be77099ce3335
add3120 <62015397+add3120@users.noreply.github.com>
2020-05-07 21:44:05 +0800

    卡牌腳本

---
+++ CardGame/Assets/Scripts/GetCard.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;  // 引用 網路連線 API
+using System.Collections;
+
+public class GetCard : MonoBehaviour
+{
+    private IEnumerator GetCardData()
+    {
+        // 引用 (網路要求 www = 網路要求.Post("網址", ""))
+        using (UnityWebRequest www = UnityWebRequest.Post("https://script.google.com/macros/s/AKfycbxQnC1WpDe1Zm-Zl5d2LofGji8okAl3fwhZO7nmZhw0ASqNcvE/exec", ""))
+        {
+            // 等待 網路要求時間
+            yield return www.SendWebRequest();
+
+            if (www.isHttpError || www.isNetworkError)
+            {
+                print("連線錯誤 :" + www.error);
+            }
+            else
+            {
+                print(www.downloadHandler.text);
+            }
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(GetCardData());
+    }
+}
